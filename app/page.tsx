6e39dc18ee2c672616a9f35b130c59cefa89a6bd
Pk <pravin.pk4068699@gmail.com>
2023-11-14 20:42:50 +0530

    tailwindcss added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,3 @@
-import styles from './page.module.css'
-
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <h2>Hello World!</h2>
-    </main>
-  )
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
